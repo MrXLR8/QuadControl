8c0032d9b7c3619eafbf2afadec5b5864a7ae750
Андрей Федоров <xlr8.19@gmail.com>
2018-07-24 10:49:00 +0300

    Async Anrdoid (no test)

--- ArduinoProject/ArduinoProject.Android/Code/Bluetooth.cs
+++ ArduinoProject/ArduinoProject.Android/Code/Bluetooth.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using Android.App;
 using Android.Bluetooth;
 using Android.Content;
@@ -16,98 +16,70 @@ namespace ArduinoProject.Shared
 {
     public static partial class Bluetooth
     {
-        const string deviceName = "HC-05";
+        
         private static BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
         private static BluetoothSocket _socket;
-        private static bool Start()
+        private static void Start()
         {
 
             if (adapter == null)
             {
                 FormAction.print("[ОШИБКА]: Bluetooth не найден");
-                return false;
+                return;
             }
             if (!adapter.IsEnabled)
             {
                 FormAction.print("[ОШИБКА]: Bluetooth выключен");
-                return false;
+                return;
             }
-            return true;
+            return;
         }
 
-        public static bool ConnectDevice()
+        public static async Task ConnectDevice()
         {
+            Start();
+
             if (adapter == null)
             {
-                if(!Start()) { return false; }
+                return;
             }
             FormAction.print("Подключаюсь к устройству");
             BluetoothDevice device =
                 (from bd in adapter.BondedDevices
-                 where bd.Name == deviceName
+                 where bd.Name == Variable.bluetoothName
                  select bd).FirstOrDefault(); // получения устройства по имени из всех спарированых
 
             if (device == null)
             {
-                FormAction.print("[ОШИБКА]: Не удалось подключиться к " + deviceName);
-                return false;
+                FormAction.print("[ОШИБКА]: Не удалось подключиться к " + Variable.bluetoothName);
+                return;
             }
             else
                 FormAction.print("Подключилься к " + device.Name); 
             _socket = device.CreateRfcommSocketToServiceRecord(Java.Util.UUID.FromString("00001101-0000-1000-8000-00805f9b34fb")); // ID Серийного соединения
-
+            
             adapter.CancelDiscovery();
             try
             {
-                _socket.Connect();
+               await _socket.ConnectAsync();
             }
             catch (Exception e)
             {
                 FormAction.print("[ОШИБКА]: Неудалось соединиться с утройством. Причина: " + e.Message);
-                return false;
+                return;
             }
-            return true;
+            return;
         }
 
-        public static bool Send(string str)
-        {
-            if(_socket==null)
-            {
-                if(!ConnectDevice())
-                {
-                    FormAction.print("[ОШИБКА]: Отправка не удалась");
-                    return false;
-                }
-            }
+     
 
-
-            FormAction.print("Отправляю строку:");
-            FormAction.print("> "+str);
-            if (_socket.IsConnected)
-            {
-
-                byte[] byteArray = Encoding.UTF8.GetBytes(str);
-                _socket.OutputStream.Write(byteArray, 0, byteArray.Length);
-                return true;
-            }
-            else
-            {
-                FormAction.print("[ОШИБКА]: Соеденение не было установлено.");
-                return false;
-            }
-            
-        }
-
-        public static bool Send(int number)
+        public static async Task Send(int number)
         {
             if (_socket == null)
             {
-                if (!ConnectDevice())
-                {
-                    FormAction.print("[ОШИБКА]: Отправка не удалась");
-                    return false;
-                }
+                 await ConnectDevice();
             }
+            
 
 
             FormAction.print("Отправляю число:");
@@ -115,14 +87,15 @@ namespace ArduinoProject.Shared
             if (_socket.IsConnected)
             {
 
-                byte[] byteArray = Encoding.UTF8.GetBytes(number.ToString());
-                _socket.OutputStream.Write(byteArray, 0, byteArray.Length);
-                return true;
+                byte _byte = Convert.ToByte(number);
+
+                _socket.OutputStream.WriteByte(_byte);
+                return;
             }
             else
             {
                 FormAction.print("[ОШИБКА]: Соеденение не было установлено.");
-                return false;
+                return;
             }
 
         }
