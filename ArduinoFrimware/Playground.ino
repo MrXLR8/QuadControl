b8c7727ded39de8429c415e31db35b7cab55d62d
Андрей Федоров <xlr8.19@gmail.com>
2018-08-05 18:09:30 +0300

    MPU Fixes

--- ArduinoFrimware/Playground.ino
+++ ArduinoFrimware/Playground.ino
@@ -1,36 +1,17 @@
 ﻿
-#include "F_LCD.h"
-#include "F_MPU6050.h"
-
-F_LCD display(20, 4);
-F_MPU6050 accel;
-
 void setup()
 {
+	
 
-	pinMode(13, OUTPUT);
-	display.start();
-	accel.start();
 	
 }
-int cords[2];
-Angle data;
-long loop_timer;
+
 void loop()
 {
 
+	Serial.println(analogRead(A0));
+	
 
-	data=accel.getGyro();
-
-
-
-	cords[0] = data.pitch;
-	cords[1] = data.roll;
-
-	display.clear();
-	display.printValues(0, cords, 3);
-
-	delay(200);
 
 }
 
