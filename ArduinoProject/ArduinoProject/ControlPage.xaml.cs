367a932793caa4c5c322fbce7f19f9f5fabaaaac
Андрей Федоров <xlr8.19@gmail.com>
2018-08-23 15:09:53 +0300

    basic joystick

---
+++ ArduinoProject/ArduinoProject/ControlPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ArduinoProject
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ControlPage : ContentPage
+	{
+		public ControlPage ()
+		{
+			InitializeComponent ();
+            up.Source = ImageSource.FromFile("arrow_idle.png");
+            down.Source = ImageSource.FromFile("arrow_idle.png");
+            rollImage.Source = ImageSource.FromFile("roll.png");
+            pitchImage.Source = ImageSource.FromFile("pitch.png");
+        }
+	}
+}
